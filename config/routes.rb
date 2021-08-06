99bfd233408760fef32d6221c082595261f95d92
richa <richayuvasoft251@gmail.com>
2021-08-06 15:36:34 +0530

    templet intrrigation

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {:sessions => "sessions",:registrations => "registrations"}
   resources :comments
   resources :likes
+  get '/search' => 'welcome#search', :as => 'search_post'
   post "/create_like_comment" => 'likes#create_like_comment', as: 'create_like_comment'
   delete "/destroy_like_comment" => 'likes#destroy_like_comment', as: 'destroy_like_comment'
 end
