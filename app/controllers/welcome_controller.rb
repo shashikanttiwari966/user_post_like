f9ed1d72b2d0e92f2b2f025a3a046dc1f651386c
shashikanttiwari966 <shashikantyuvasoft311@gmail.com>
2021-08-06 15:36:34 +0530

    templet intrrigation

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,16 @@ class WelcomeController < ApplicationController
   def index
    @posts = Post.paginate(page: params[:page], per_page: 6)
    @postall = Post.all
+   # @post = Post.search(params[:search])
+  end
+
+  def search
+    # debugger
+    if params[:search].blank?  
+      redirect_to(root_path, alert: "Empty field!") and return  
+    else  
+      @parameter = params[:search].downcase  
+      @results = Post.all.where("lower(title) LIKE :search", search: "%#{@parameter}%").paginate(page: params[:page], per_page: 3)  
+    end 
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes, as: :like_on
+
 end
